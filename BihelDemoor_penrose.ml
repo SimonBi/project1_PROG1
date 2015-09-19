400ac43402beb3ea274d2f61d356f70eb632c012
Simon Bihel <simon.bihel@outlook.com>
2015-09-20 00:18:22 +0200

    init
    Changes to be committed:
    new file:   BihelDemoor_hanoi.ml
    new file:   BihelDemoor_penrose.ml

---
+++ BihelDemoor_penrose.ml
@@ -0,0 +1,24 @@
+(*
+Author: BIHEL Simon, De-Moor Florestan
+Institute: ENS Rennes
+*)
+
+open Graphics;;
+
+(* Close any possible open window *)
+close_graph ();;
+
+(* Open a 800x600 window. Make sure to include a space at the beginning of the heometry specification *)
+open_graph " 800x600-0+0";;
+
+(* Move to point 50x100 *)
+moveto 50 100;;
+
+(* Draw a line to point 300x150 *)
+lineto 300 150;;
+
+(* Set the filling color: red, blue, yellow, cyan, magenta, etc. *)
+set_color green;;
+
+(* Fill the polygon defined by points 0x0, 10x50 and 50x100 *)
+fill_poly [|(0,0);(10,50);(50,10)|];;
