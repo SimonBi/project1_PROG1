bf3d08e8ffe7ac1a5cb5a69036f645e31e9b2227
Simon Bihel <simon.bihel@outlook.com>
2015-09-28 00:39:08 +0200

    star of obtuse triangles

--- penrose_extended.ml
+++ penrose_extended.ml
@@ -10,6 +10,7 @@ Second version: advanced features.
 #load "unix.cma"
 open Unix;;
 open Graphics;;
+open List;;
 
 close_graph();;
 open_graph " 1000*1000";;
@@ -21,10 +22,12 @@ let pi = 4. *. atan (1.);;
 
 (** Variables that can be changed. *)
 let wait_each_triangle = false;;
-let nb_generations = 5;;
+let nb_generations = 6;;
 let show_each_generation = true;;
 let robinson_single_triangle = false;;
 let robinson_wheel_triangles = true;;
+let type_wheel_triangles = Obtuse;; 
+    (* Acute triangles create a wheel, obtuse create a star. *)
 
 
 (** Sleep function. *)
@@ -42,6 +45,8 @@ let draw points t_type =
   moveto (fst points.(0)) (snd points.(0));
   set_color black;
   set_line_width 2;
+  (* Only two borders of a triangle are drawn to create new shapes
+  when two triangles are side-by-side. *)
   if t_type = Obtuse then begin
     lineto (fst points.(1)) (snd points.(1));
     lineto (fst points.(2)) (snd points.(2));
@@ -100,7 +105,7 @@ let rec divide generation points t_type =
        divide (generation-1) new_triangles.(2) Obtuse
   end;;
 
-
+(** Create a single obtuse triangle and call divide for it. *)
 if robinson_single_triangle then 
   let y = sin(pi /. 5.) in
   let points = [|(0, 0); (int_of_float(150. *. phi), int_of_float(300. *. y));
@@ -113,35 +118,84 @@ if robinson_single_triangle then
   else divide nb_generations points Obtuse;;
 
 
-(** Build a wheel of acute triangles and call divide for each of them. *)
+let point_closer_center point center = 
+  (int_of_float(center +. (float_of_int(fst point) -. center) /. phi), 
+   int_of_float(center +. (float_of_int(snd point) -. center) /. phi));;
+
+
+(** Build a wheel of identical triangles and call divide for each of them. *)
 if robinson_wheel_triangles then begin
-  let t_type = Acute
+  let t_type = type_wheel_triangles
   and triangles = ref [] in
   let zoom = 250. in begin
+    (* Build the triangles of the wheel.
+       For each for loop we create a triangle and it's mirror one. *)
     for i = 1 to 5 do
       let fi = float_of_int i 
       and center = 300. in
       let intC = int_of_float center in
+
       let point1 = (int_of_float(center +. (zoom *. cos((fi -. 1.)  *.  pi /. 5.))),
                     int_of_float(center +. (zoom *. sin((fi -. 1.)  *.  pi /. 5.))))
       and point2 = (int_of_float(center +. (zoom *. cos(fi  *.  pi /. 5.))),
                     int_of_float(center +. (zoom *. sin(fi  *.  pi /. 5.)))) in
-      begin
-        if i mod 2 = 0 then 
-          triangles := ( !triangles) @ [[|(intC, intC); point1; point2|];
-                                        [|(intC, intC); (-1 * (fst point2) + 2 * intC, -1 * (snd point2) + 2 * intC); 
-                                          (-1 * (fst point1) + 2 * intC, -1 * (snd point1) + 2 * intC)|]]
-        else
-          triangles := ( !triangles) @ [[|(intC, intC); point2; point1|];
-                                        [|(intC, intC); (-1 * (fst point1) + 2 * intC, -1 * (snd point1) + 2 * intC); 
-                                          (-1 * (fst point2) + 2 * intC, -1 * (snd point2) + 2 * intC)|]]
+      let obtuse_point1 = point_closer_center point1 center 
+      and obtuse_point2 = point_closer_center point2 center in
+
+      if t_type = Acute then begin
+          if i mod 2 = 0 then 
+            triangles := ( !triangles) @ [[|(intC, intC); point1; point2|];
+                                          [|(intC, intC);
+                                            (-1 * (fst point2) + 2 * intC,
+                                             -1 * (snd point2) + 2 * intC); 
+                                            (-1 * (fst point1) + 2 * intC,
+                                             -1 * (snd point1) + 2 * intC)|]]
+          else
+            triangles := ( !triangles) @ [[|(intC, intC); point2; point1|];
+                                          [|(intC, intC);
+                                            (-1 * (fst point1) + 2 * intC, 
+                                             -1 * (snd point1) + 2 * intC); 
+                                            (-1 * (fst point2) + 2 * intC, 
+                                             -1 * (snd point2) + 2 * intC)|]]
+        end
+      (* Reversing an obtuse triangle to eventually create a star 
+         is a little more complicated. *)
+      else begin
+          if i mod 2 = 0 then 
+            triangles := ( !triangles) @ [[|(intC, intC);
+                                            obtuse_point1;
+                                            point2|];
+                                          [|(intC, intC);
+                                            (-1 * (fst obtuse_point2) + 2 * intC,
+                                             -1 * (snd obtuse_point2) + 2 * intC); 
+                                            (-1 * (fst point1) + 2 * intC,
+                                             -1 * (snd point1) + 2 * intC)|]]
+          else
+            triangles := ( !triangles) @ [[|(intC, intC); obtuse_point2; point1|];
+                                          [|(intC, intC);
+                                            (-1 * (fst obtuse_point1) + 2 * intC, 
+                                             -1 * (snd obtuse_point1) + 2 * intC); 
+                                            (-1 * (fst point2) + 2 * intC, 
+                                             -1 * (snd point2) + 2 * intC)|]]
+        
       end
     done;
-
-    while !triangles <> [] do
-      divide nb_generations (List.hd !triangles) t_type;
-      triangles := (List.tl !triangles)
-    done
+  
+    (* Drawing of the triangles. *)
+    if show_each_generation then
+      for i = 0 to nb_generations do
+        let triangles_copy = ref !triangles in
+        while !triangles_copy <> [] do
+          divide i (hd !triangles_copy) t_type;
+          triangles_copy := (tl !triangles_copy)
+        done;
+        minisleep 0.2
+      done
+    else
+      while !triangles <> [] do
+        divide nb_generations (hd !triangles) t_type;
+        triangles := (tl !triangles)
+      done;
   end
   end;;
 
